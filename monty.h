462a519b5b65c87a5bc7c6547976ba8c62c79efe
belaygithub <101358546+belaygithub@users.noreply.github.com>
2022-07-14 22:51:47 +0300

    update monty.h

--- monty.h
+++ monty.h
@@ -1,32 +1,39 @@
-#ifndef MONTY_H
-#define MONTY_H
-#define _POSIX_C_SOURCE  200809L
-#define _GNU_SOURCE
-#include <string.h>
-#include <stdarg.h>
+#ifndef _MONTY_
+#define _MONTY_
+
+/* Constants */
+#define SUCSS_OP		0
+#define VALID_PARM		0
+#define MIN_ARGS		2
+#define METH_STACK		300
+#define METH_QUEUE		301
+
+/* Common Errors */
+#define ERR_BAD_INST	100
+#define ERR_BAD_MALL	101
+#define ERR_INVLD_PARM	102
+
+/* Usage Errors */
+#define ERR_ARG_USG		200
+#define ERR_PUSH_USG	201
+#define ERR_PINT_USG	202
+#define ERR_POP_USG		203
+#define ERR_SWAP_USG	204
+#define ERR_ADD_USG		205
+#define ERR_SUB_USG		206
+#define ERR_DIV_USG		207
+#define ERR_DIV_ZRO		208
+#define ERR_MUL_USG		209
+#define ERR_MOD_USG		210
+#define ERR_PCH_USG		211
+#define ERR_PCH_EMP		212
+
 #include <ctype.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
-#define error_ac() fprintf(stderr, "USAGE: monty file\n")
-#define error_fopen(name) fprintf(stderr, "Error: Can't open file %s\n", name)
-#define error_malloc() fprintf(stderr, "Error: malloc failed\n")
-#define error_op(ln, c) fprintf(stderr, "L%u: unknown instruction %s\n", ln, c)
-#define error_int(ln) fprintf(stderr, "L%u: usage: push integer\n", ln)
-#define error_pint(ln) fprintf(stderr, "L%u: can't pint, stack empty\n", ln)
-#define error_pop(ln) fprintf(stderr, "L%u: can't pop an empty stack\n", ln)
-#define error_swap(l) fprintf(stderr, "L%u: can't swap, stack too short\n", l)
-#define error_add(ln) fprintf(stderr, "L%u: can't add, stack too short\n", ln)
-#define error_sub(ln) fprintf(stderr, "L%u: can't sub, stack too short\n", ln)
-#define error_div1(ln) fprintf(stderr, "L%u: can't div, stack too short\n", ln)
-#define error_div2(ln) fprintf(stderr, "L%u: division by zero\n", ln)
-#define error_mul(ln) fprintf(stderr, "L%u: can't mul, stack too short\n", ln)
-#define error_mod1(ln) fprintf(stderr, "L%u: can't mod, stack too short\n", ln)
-#define err_pr1(l) fprintf(stderr, "L%u: can't pchar, value out of range\n", l)
-#define error_pchar2(l) fprintf(stderr, "L%u: can't pchar, stack empty\n", l)
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -35,13 +42,13 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-int n;
-struct stack_s *prev;
-struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -50,21 +57,31 @@ struct stack_s *next;
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
-
 typedef struct instruction_s
 {
-char *opcode;
-void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern stack_t *h;
-extern FILE *file;
-extern char *value;
-extern int mode;
+extern stack_t *head;
 
-void push(stack_t **stack, unsigned int line_number);
+void check_args_num(int argn);
+FILE *open_file(char *filename);
+void check_access_rights(char *filename);
+int check_push_param(char *param);
+int check_digits(char *s);
+void frees_stack(void);
+int handle_execution(char *op_code, char *op_param, unsigned int line, int m);
+void handle_error(int errno, char *opcode, unsigned int line, char *buff);
+void handle_cerror(int errno, char *opcode, unsigned int line);
+void handle_uerror(int errno, unsigned int line);
+void handle_more_uerror(int errno, unsigned int line);
+void (*pick_func(char *s))(stack_t **, unsigned int);
+unsigned int count_stack(stack_t *stack);
+void push(stack_t **stack, unsigned int param);
+void push_queue(stack_t **stack, unsigned int param);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
@@ -72,19 +89,12 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
-void divt(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
-void stack_queue(stack_t **stack, unsigned int line_number);
-size_t dlistint_len(const stack_t *h);
-stack_t *add_dnodeint(stack_t **head, const int n);
-stack_t *add_dnodeint_end(stack_t **head, const int n);
-void free_dlistint(stack_t *head);
-stack_t *get_dnodeint_at_index(stack_t *head, unsigned int index);
-stack_t *insert_dnodeint_at_index(stack_t **h, unsigned int idx, int n);
-int delete_dnodeint_at_index(stack_t **head, unsigned int index);
+
 #endif
