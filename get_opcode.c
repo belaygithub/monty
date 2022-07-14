d09d7a1e03c7441a22980a0800f292a05d7cd63f
belaygit <belayeshetu2014@gmail.com>
2022-07-14 09:47:03 -0700

    fixed get_opcode.c

--- get_opcode.c
+++ get_opcode.c
@@ -1,40 +1,28 @@
 #include "monty.h"
 /**
- * get_opcode_func - selects the correct function to perform the opcode
- * @s: opcode passed to the program
- * Return: a pointer to the function given the opcode or NULL if not found
+ * get_op_func - find the appropriate function given an instruction
+ * from a byte code line
+ * @line_opcode: opcode read from line
+ * Return: pointer to the function that corresponds to the opcode
  */
-void (*get_opcode_func(char *s))(stack_t **stack, unsigned int ln)
+void (*get_op_func(char *line_opcode))(stack_t **, unsigned int)
 {
-int i;
-instruction_t stk[] = {
-			 {"pall", stk_pall},
-			 {"push", stk_push},
-			 {"pop", stk_pop},
-			 {"add", stk_add},
-			 {"swap", stk_swap},
-			 {"pint", stk_pint},
-			 {"nop", stk_nop},
-			 {"pchar", stk_pchar},
-			 {"pstr", stk_pstr},
-			 {"rotl", stk_rotl},
-			 {"rotr", stk_rotr},
-			 {"sub", stk_sub},
-			 {"div", stk_div},
-			 {"mul", stk_mul},
-			 {"mod", stk_mod},
-			 {"queue", stk_queue},
-			 {"stack", stk_stack},
-			 {NULL, NULL}
+instruction_t instruct[] = {
+			      {"push", push},
+			      {"pall", pall},
+			      {"pint", pint},
+			      {"pop", pop_int},
+			      {"swap", swap_int},
+			      {"add", add_ints},
+			      {"nop", nop_nop},
+			      {NULL, NULL}
 };
+int i = 0;
 
-i = 0;
-while (i < 17)
+while (instruct[i].opcode)
 {
-if (strcmp(s, (stk[i]).opcode) == 0)
-{
-return (*(stk[i]).f);
-}
+if (strcmp(line_opcode, instruct[i].opcode) == 0)
+return (instruct[i].f);
 i++;
 }
 return (NULL);
